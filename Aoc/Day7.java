2f64f889a56ea1a883008fe8bea724a132b38d65
oLo <OliwerDrywa@gmail.com>
2024-12-07 18:00:49 +0100

    7

---
+++ Aoc/Day7.java
@@ -0,0 +1,70 @@
+package Aoc;
+
+import java.util.Arrays;
+
+public class Day7 {
+
+    public static long Part1(String input) {
+        String[] lines = input.split("\n");
+
+        long total = 0;
+        for (String line : lines) {
+            String[] sections = line.split(": ");
+            long targetResult = Long.parseLong(sections[0]);
+            int[] nums = Arrays.stream(sections[1].split(" ")).mapToInt(Integer::valueOf).toArray();
+
+            for (int i = 0; i < Math.pow(2, nums.length - 1); i++) {
+                String binary = String.format("%" + (nums.length - 1) + "s", Integer.toBinaryString(i)).replace(' ', '0');
+
+                long sum = nums[0];
+                for (int j = 0; j < binary.length(); j++) {
+                    if (binary.charAt(j) == '0') {
+                        sum += nums[j + 1];
+                    } else if (binary.charAt(j) == '1') {
+                        sum *= nums[j + 1];
+                    }
+                }
+                if (sum == targetResult) {
+                    total += sum;
+                    break;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    public static long Part2(String input) {
+        String[] lines = input.split("\n");
+
+        long total = 0;
+        for (String line : lines) {
+            String[] sections = line.split(": ");
+            long targetResult = Long.parseLong(sections[0]);
+            int[] nums = Arrays.stream(sections[1].split(" ")).mapToInt(Integer::valueOf).toArray();
+
+            for (int i = 0; i < Math.pow(3, nums.length - 1); i++) {
+                String combinations = String.format("%" + (nums.length - 1) + "s", Integer.toString(Integer.parseInt(Integer.toString(i), 10), 3)).replace(' ', '0');
+
+                long sum = nums[0];
+                for (int j = 0; j < combinations.length(); j++) {
+                    switch (combinations.charAt(j)) {
+                        case '0' ->
+                            sum += nums[j + 1];
+                        case '1' ->
+                            sum *= nums[j + 1];
+                        case '2' ->
+                            sum = Long.parseLong("" + sum + nums[j + 1]);
+                    }
+                }
+                if (sum == targetResult) {
+                    total += sum;
+                    break;
+                }
+            }
+        }
+
+        return total;
+    }
+
+}
--- Today.java
+++ Today.java
@@ -7,21 +7,20 @@ import java.nio.file.Paths;
 public class Today {
 
     public static void main(String[] args) {
-        String input = loadDataFile("d6_input");
+        String input = loadDataFile("d7_input");
         // String input = """
-        //                ....#.....
-        //                .........#
-        //                ..........
-        //                ..#.......
-        //                .......#..
-        //                ..........
-        //                .#..^.....
-        //                ........#.
-        //                #.........
-        //                ......#...
+        //                190: 10 19
+        //                3267: 81 40 27
+        //                83: 17 5
+        //                156: 15 6
+        //                7290: 6 8 6 15
+        //                161011: 16 10 13
+        //                192: 17 8 14
+        //                21037: 9 7 18 13
+        //                292: 11 6 16 20
         //                """;
 
-        System.out.println("today's answer: " + Aoc.Day6.Part1(input));
+        System.out.println("today's answer: " + Aoc.Day7.Part2(input));
     }
 
     public static String loadDataFile(String path) {
